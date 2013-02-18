5478400f5291de77d419be821837d6683d15323b
Janmejay Rai <janmejay.rai@intellecap.net>
2013-02-18 11:43:20 +0530

    Initial Commit - Trip Advisor

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TripPlanner::Application.config.secret_token = '26cc3c4e2fe882a31a96e8954f325f0ebb8a95b19073ae87f59e21a69be75b4a64f8686b396b3e1db732444f02f9f68702f6aa5584bb387601b96689d964664b'
